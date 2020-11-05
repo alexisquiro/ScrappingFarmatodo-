f1547ae586b7bd66ddfdccac655761d67e5464ed
Alexis Quiros <sixela.quiros@gmail.com>
2020-11-05 09:57:04 -0400

    ya incorpora ciudad principal

--- farmatodoIndex.py
+++ farmatodoIndex.py
@@ -6,10 +6,26 @@ import requests
 import pandas as pd
 import time 
 
-
 driver= webdriver.Firefox(executable_path=r"/home/alexis/Documents/geckodriver")
+
+
+def get_url(driver,url):
+    driver.get(url)
+
+
+
+def get_locations(driver,url):
+    driver.execute_script("window.open('');")
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get(url)
+    time.sleep(15)
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+
 url="https://farmatodo.com.ve/categorias/belleza/cosmeticos"
-driver.get(url)
+
+get_url(driver,url)
+
 #tiempo de espera le puse 10 seg porque mi internet no es tan rapido
 time.sleep(10)
 
@@ -52,7 +68,7 @@ for c in cards:
     images.append(picture['src'])
     #extraemos el nombre 
     name=pageSoup.find('p',class_='text-title')
-    names.append(name.text)
+    names.append(name.textr)
     #extraemos la descripcion 
     descr=pageSoup.find('p',class_='text-description')
     description.append(descr.text)
@@ -66,23 +82,52 @@ for c in cards:
     #aqui quitamos los escios en blanco del nombre para la busqueda
     urlProduct.append(urlSearch + '' +  name.text.replace(" ", "%20") +'&departamento=Todos&filtros=')
 
+time.sleep(15)
+numCities=list()
+mainCity=list()
+cont=0
+#se abre una nueva pestaña con cada producto y se hace click para extraer su inf de locacion 
+for url in urlProduct:
+    driver.execute_script("window.open('');")
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get(url)
+    time.sleep(15)
+    element=driver.find_elements_by_xpath("/html/body/app-root/div/div[2]/app-algolia/div/div/div/app-group-view/div/div[2]/div[2]/div[1]")
+    element[0].click()
+    time.sleep(15)
+    #aqui se recoge la info de la ubicacion
+    #convierto el elemento de selenium y extraigo su html
+    page=driver.page_source
+    pageSoup= BeautifulSoup(page,'html.parser') 
+    
+    #se extrae el numero de ciudades
+    #cities=pageSoup.find('div',class_='ng-star-inserted')
+    maincity=pageSoup.find('h6',class_='text-city')
+    print(maincity)
+    #numCities.append(str(len(cities)))
+    #se extrae la ciudad principal 
+    mainCity.append(maincity.text)
+    time.sleep(15)
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
 
 
 
-print (prices)
+print (mainCity)
 
-print(len(images))
+print(numCities)
 
+#aqui extraeremos los datos de ubicacion del proeducto
 
 
 
 
 #transformamos toda la data que recogimos y la metemos en el csv 
-df=pd.DataFrame({'Name':names,'Description':description,'Prices':prices,'Availability':disp,'Image':images,'URL':urlProduct})
+df=pd.DataFrame({'Name':names,'Description':description,'Prices':prices,'Availability':disp,'Image':images,'URL':urlProduct,'Main':mainCity})
 
 #print(df)
 
 #salvamos el csv 
-df.to_csv('farmatodo_belleza_cosmeticos.csv')
+df.to_csv('farmatodo_belleza_cosmeticos2.csv')
 
 
